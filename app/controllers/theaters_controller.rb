aa99832dda2e13ef633c43bcf2a9e41326a7265c
Kazuki Kondo <k.kazuki526@gmail.com>
2021-01-01 08:53:16 +0900

    create_theater

--- app/controllers/theaters_controller.rb
+++ app/controllers/theaters_controller.rb
@@ -1,4 +1,6 @@
 class TheatersController < ApplicationController
+  before_action :set_theater, only: [:show, :edit, :update, :destroy]
+
   def index
     @theaters = Theater.all
   end
@@ -11,14 +13,33 @@ class TheatersController < ApplicationController
   end
 
   def create
+    @theater = Theater.new(theater_params)
+    if @theater.save
+      redirect_to theaters_path
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    @theater.update(theater_params)
+    redirect_to theater_path(@theater)
   end
 
   def destroy
+    @theater.destroy
+    redirect_to theaters_path
   end
+
+  private
+    def theater_params
+      params.require(:theater).permit(:title, :writer, :country, :man, :female, :translator, :include, :publication, :stage_id)
+    end
+
+    def set_theater
+      @theater = Theater.find(params[:id])
+    end
 end
