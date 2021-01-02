bbeabcc76707919e17f49ad0e892e953f86ca1b6
Kazuki Kondo <k.kazuki526@gmail.com>
2021-01-02 20:37:31 +0900

    user signin form

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module ActorConnection
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # not add field_with_errors when validate error
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
   end
 end
