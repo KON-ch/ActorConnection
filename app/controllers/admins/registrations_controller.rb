9b0bbc7570610cb6afee8ee31a896b1353cf73b1
Kazuki Kondo <k.kazuki526@gmail.com>
2021-02-01 22:27:41 +0900

    create admin

--- app/controllers/admins/registrations_controller.rb
+++ app/controllers/admins/registrations_controller.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Admins::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   # def new
@@ -10,9 +10,10 @@ class Admins::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    byebug
+    super
+  end
 
   # GET /resource/edit
   # def edit
@@ -20,9 +21,9 @@ class Admins::RegistrationsController < Devise::RegistrationsController
   # end
 
   # PUT /resource
-  # def update
-  #   super
-  # end
+  def update
+    super
+  end
 
   # DELETE /resource
   # def destroy
@@ -38,17 +39,17 @@ class Admins::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmatin])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :password, :password_confirmatin])
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   
   devise_for :admins, :controllers => {
+    :registrations => 'admins/registrations',
     :sessions => 'admins/sessions'
   }
 
@@ -9,6 +10,8 @@ Rails.application.routes.draw do
     get "dashboard/login", :to => "admins/sessions#new"
     post "dashboard/login", :to => "admins/sessions#create"
     get "dashboard/logout", :to => "admins/sessions#destroy"
+    get "dashboard/signup", :to => "admins/registrations#new"
+    post "dashboard/signup", :to => "admins/registrations#create"
   end
 
   namespace :dashboard do
