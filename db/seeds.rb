67098667b887aec3afb112de469e381286e358ab
Kazuki Kondo <k.kazuki526@gmail.com>
2021-02-01 21:20:58 +0900

    edit user new form

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-User.create(:name => "こん", :birthday => 19930526, :sex => 0, :email => "kon@example.com", :encrypted_password => "12345678")
-Admin.create(:name => "admin", :email => "admin@example.com", :encrypted_password => "12345678")
