d1bc6207f26a83cb472351437d422420db88e6fd
Kazuki Kondo <k.kazuki526@gmail.com>
2021-02-24 11:04:21 +0900

    edit top page

--- app/controllers/webs_controller.rb
+++ app/controllers/webs_controller.rb
@@ -3,5 +3,6 @@ class WebsController < ApplicationController
     @movies = Movie.order(likers_count: :desc).limit(3)
     @review = nil
     @stages = Stage.where('end_date > ?', Date.today).order(start_date: :asc).limit(4)
+    @tags = Tag.all.limit(12)
   end
 end
