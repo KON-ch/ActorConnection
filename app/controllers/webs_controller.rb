a794a3ef580586771bda6857052651499bf2f2e4
Kazuki Kondo <k.kazuki526@gmail.com>
2021-03-02 16:05:10 +0900

    add slick to web and movie

--- app/controllers/webs_controller.rb
+++ app/controllers/webs_controller.rb
@@ -1,6 +1,6 @@
 class WebsController < ApplicationController
   def index
-    @movies = Movie.order(likers_count: :desc).limit(3)
+    @movies = Movie.order(likers_count: :desc).limit(6)
     @review = nil
     @stages = Stage.where('end_date > ?', Date.today).order(start_date: :asc).limit(4)
     @tags = Tag.all.limit(12)
