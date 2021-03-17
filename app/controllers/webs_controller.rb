21bb2da84843e954c6f0633403517c83298e2cf5
KON-ch <k.kazuki@gmial.com>
2021-03-17 23:00:15 +0000

    change card to slick

--- app/controllers/webs_controller.rb
+++ app/controllers/webs_controller.rb
@@ -2,7 +2,7 @@ class WebsController < ApplicationController
   def index
     @movies = Movie.order(likers_count: :desc).limit(6)
     @review = nil
-    @stages = Stage.where(extract(month from end_date) = ?, Date.current.month).order(start_date: :asc).limit(4)
+    @stages = Stage.where('extract(month from end_date) = ?', Date.current.month).order(start_date: :asc).limit(4)
     @tags = Tag.all.limit(12)
   end
 end
