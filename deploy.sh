65eedde405e3fb8f885dc7786e8aafb8ab42a249
Kazuki Kondo <k.kazuki526@gmail.com>
2021-03-25 09:24:21 +0900

    ready cd 38

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 set +e
 cd /var/www/ActorConnection/ActorConnection
 git pull
+gem install bundler -v 2.2.3
 bundle install --without test development
 bundle exec rails assets:precompile RAILS_ENV=production
 bundle exec rails db:migrate RAILS_ENV=production
