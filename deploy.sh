a562de19cec9183954cf7c577118c991d355e685
Kazuki Kondo <k.kazuki526@gmail.com>
2021-03-26 23:42:41 +0900

    ready cd 68

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 set +e
 cd /var/www/ActorConnection/ActorConnection
 git pull
-bin/bundle install --without test development
-bin/bundle exec rails assets:precompile RAILS_ENV=production
-bin/bundle exec rails db:migrate RAILS_ENV=production
+bundle install --without test development
+bundle exec rails assets:precompile RAILS_ENV=production
+bundle exec rails db:migrate RAILS_ENV=production
 cat tmp/pids/server.pid |xargs kill -9
-bin/bundle exec rails s -e production
+bundle exec rails s -e production
