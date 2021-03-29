674c173d0711d58ce34e6e220eeab2cbd9cf56b5
KON-ch <k.kazuki526@gmail.com>
2021-03-29 03:26:19 +0000

    ready sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Sentry.init do |config|
-  config.dsn = 'https://e0b96cce2cb243678d6f5e3aa5d2106a@o560024.ingest.sentry.io/5695320'
+  config.dsn = ENV["SENTRY_DSN"]
   config.breadcrumbs_logger = [:active_support_logger]
 
   # Set tracesSampleRate to 1.0 to capture 100%
