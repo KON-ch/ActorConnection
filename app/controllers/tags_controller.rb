5bf71252651ef0b6a389d819af515ef1030d0287
Kazuki Kondo <k.kazuki526@gmail.com>
2021-04-03 20:08:04 +0900

    use bullet to tag

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,6 +3,8 @@
 class TagsController < ApplicationController
   def show
     @tag = Tag.find(params[:id])
+    @tag_stages = @tag.stages.preload(:theater, :place, :stage_tags, :tags, :reviews)
+    @tag_movies = @tag.movies.preload(:country, :movie_tags, :tags, :reviews)
     stages = @tag.stages
     @stage_review = current_user.reviews.stages(stages)
     movies = @tag.movies
