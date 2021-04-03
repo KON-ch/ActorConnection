666186d5c37823d02214bf2e517efca69cd31034
Kazuki Kondo <k.kazuki526@gmail.com>
2021-04-03 20:09:25 +0900

    use bullet to web

--- app/controllers/webs_controller.rb
+++ app/controllers/webs_controller.rb
@@ -2,7 +2,7 @@
 
 class WebsController < ApplicationController
   def index
-    @movies = Movie.order(likers_count: :desc).limit(6)
+    @movies = Movie.preload(:tags, :country).order(likers_count: :desc).limit(6)
     @review = nil
     @stages = Stage.where('extract(month from end_date) = ?', Date.current.month).order(start_date: :asc).limit(4)
     @tags = Tag.all.limit(12)
