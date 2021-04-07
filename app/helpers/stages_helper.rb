099efcace9a6667a063ac213591c5e400c4d986c
Kazuki Kondo <k.kazuki526@gmail.com>
2021-04-07 11:32:30 +0900

    stagesのリファクタ

--- app/helpers/stages_helper.rb
+++ app/helpers/stages_helper.rb
@@ -5,4 +5,16 @@ module StagesHelper
     movie_ids = MovieTag.where(tag_id: tag_id).limit(3).pluck(:movie_id)
     Movie.preload(:country, :movie_tags, :tags, :reviews).find(movie_ids)
   end
+
+  def stage_playing(stage)
+    stage.start_date <= Date.today && stage.end_date >= Date.today
+  end
+
+  def stage_finished(stage)
+    stage.end_date < Date.today
+  end
+
+  def stage_14days_ago(stage)
+    stage.start_date - Date.today <= 14 && stage.start_date - Date.today > 0
+  end
 end
